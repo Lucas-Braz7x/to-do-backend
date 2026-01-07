8166421dd963ded03a1680949bbb877e545d18f7
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2026-01-07 00:23:36 -0300

    lint: fix lint

--- src/modules/tasks/entities/task.entity.ts
+++ src/modules/tasks/entities/task.entity.ts
@@ -1,5 +1,12 @@
 import { TaskStatus } from '@prisma/client';
-import { IsDate, IsEnum, IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 import { AuditCreatedUpdated } from '../../users/entities/user.entity';
 
 export class Task extends AuditCreatedUpdated {
